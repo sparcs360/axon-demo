2383fb7ba263e8e686d04931a155daf90a5744b3
Lee Newfeld <lee.newfeld@gmail.com>
2017-09-05 08:10:19 +0100

    Subscribe to events on the distributed event bus

--- axon-demo-counter/src/main/java/com/sparcs/counter/config/DistributedEventBusAmqpConfig.java
+++ axon-demo-counter/src/main/java/com/sparcs/counter/config/DistributedEventBusAmqpConfig.java
@@ -1,11 +1,7 @@
 package com.sparcs.counter.config;
 
 import org.axonframework.amqp.eventhandling.spring.SpringAMQPMessageSource;
-import org.axonframework.eventhandling.EventBus;
-import org.axonframework.eventhandling.EventHandler;
-import org.axonframework.eventhandling.EventHandlerInvoker;
-import org.axonframework.eventhandling.EventProcessor;
-import org.axonframework.eventhandling.SubscribingEventProcessor;
+import org.axonframework.config.EventHandlingConfiguration;
 import org.axonframework.serialization.Serializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,86 +19,76 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.rabbitmq.client.Channel;
-import com.sparcs.kiosk.executive.account.EBalanceChanged;
 
 @Configuration
 public class DistributedEventBusAmqpConfig {
 
-	private static final Logger LOG = LoggerFactory.getLogger(DistributedEventBusAmqpConfig.class);
+    private static final Logger LOG = LoggerFactory.getLogger(DistributedEventBusAmqpConfig.class);
 
-	@Autowired
-	private CounterProperties counterProperties;
+    @Autowired
+    private CounterProperties counterProperties;
 
-	@Bean
-	@Profile("!DisableAmqp")
-	public AmqpAdmin distributedEventBusAdmin(ConnectionFactory amqpConnectionFactory) {
+    @Bean
+    @Profile("!DisableAmqp")
+    public AmqpAdmin distributedEventBusAdmin(ConnectionFactory amqpConnectionFactory) {
 
         LOG.debug("distributedEventBusAdmin(amqpConnectionFactory={})", amqpConnectionFactory);
 
-		RabbitAdmin admin = new RabbitAdmin(amqpConnectionFactory);
-		admin.setAutoStartup(true);
-		admin.declareExchange(distributedEventBusExchange());
-		admin.declareQueue(distributedEventBusQueue());
-		admin.declareBinding(distributedEventBusBinding());
-		return admin;
-	}
+        RabbitAdmin admin = new RabbitAdmin(amqpConnectionFactory);
+        admin.setAutoStartup(true);
+        admin.declareExchange(distributedEventBusExchange());
+        admin.declareQueue(distributedEventBusQueue());
+        admin.declareBinding(distributedEventBusBinding());
+        return admin;
+    }
 
-	@Bean
-	FanoutExchange distributedEventBusExchange() {
+    @Bean
+    FanoutExchange distributedEventBusExchange() {
 
         LOG.debug("distributedEventBusExchange() <- getAmqpEventbusExchangeName={}", counterProperties.getAmqpEventbusExchangeName());
 
-		FanoutExchange exchange = new FanoutExchange(counterProperties.getAmqpEventbusExchangeName(), true, false);
-		return exchange;
-	}
-
-	@Bean
-	public Queue distributedEventBusQueue() {
-
-		LOG.debug("distributedEventBusQueue() <- queueName={}", counterProperties.getAmqpEventbusQueueName());
-
-		Queue queue = new Queue(counterProperties.getAmqpEventbusQueueName(), false, false, true);
-		return queue;
-	}
-
-	@Bean
-	Binding distributedEventBusBinding() {
-
-		LOG.debug("distributedEventBusBinding() <- queueName={}, exchangeName={}",
-				counterProperties.getAmqpEventbusQueueName(), counterProperties.getAmqpEventbusExchangeName());
-
-		Binding binding = new Binding(counterProperties.getAmqpEventbusQueueName(), Binding.DestinationType.QUEUE,
-				counterProperties.getAmqpEventbusExchangeName(), "", null);
-		return binding;
-	}
-    
-	@Bean
-	public SpringAMQPMessageSource distributedEventBusMessageSource(/* EventBus eventBus, */Serializer serializer) {
-
-		SpringAMQPMessageSource messageSource = new SpringAMQPMessageSource(serializer) {
-
-			@RabbitListener(queues="#{@counterProperties.amqpEventbusQueueName}")
-			@Override
-			public void onMessage(Message message, Channel channel) throws Exception {
-
-				super.onMessage(message, channel);
-			}
-		};
-		messageSource.subscribe(events -> events.forEach(event -> LOG.trace(event.getPayload().toString())));
-		//messageSource.subscribe(events -> eventBus.publish(events));
-		return messageSource;
-	}
+        FanoutExchange exchange = new FanoutExchange(counterProperties.getAmqpEventbusExchangeName(), true, false);
+        return exchange;
+    }
 
     @Bean
-    public EventProcessor eventProcessor(EventBus eventBus) {
-        EventProcessor eventProcessor = new SubscribingEventProcessor("eventProcessor", (EventHandlerInvoker) this, eventBus);
-        eventProcessor.start();
-        return eventProcessor;
+    public Queue distributedEventBusQueue() {
+
+        LOG.debug("distributedEventBusQueue() <- queueName={}", counterProperties.getAmqpEventbusQueueName());
+
+        Queue queue = new Queue(counterProperties.getAmqpEventbusQueueName(), false, false, true);
+        return queue;
     }
-    
-	@EventHandler
-	void on(EBalanceChanged event) {
-		
-		LOG.info("{}", event);
-	}
+
+    @Bean
+    Binding distributedEventBusBinding() {
+
+        LOG.debug("distributedEventBusBinding() <- queueName={}, exchangeName={}",
+                counterProperties.getAmqpEventbusQueueName(), counterProperties.getAmqpEventbusExchangeName());
+
+        Binding binding = new Binding(counterProperties.getAmqpEventbusQueueName(), Binding.DestinationType.QUEUE,
+                counterProperties.getAmqpEventbusExchangeName(), "", null);
+        return binding;
+    }
+
+    @Bean
+    public SpringAMQPMessageSource distributedEventBusMessageSource(Serializer serializer) {
+
+        SpringAMQPMessageSource messageSource = new SpringAMQPMessageSource(serializer) {
+
+            @RabbitListener(queues = "#{@counterProperties.amqpEventbusQueueName}")
+            @Override
+            public void onMessage(Message message, Channel channel) throws Exception {
+
+                super.onMessage(message, channel);
+            }
+        };
+        return messageSource;
+    }
+
+    @Autowired
+    public void configure(EventHandlingConfiguration config, SpringAMQPMessageSource source) {
+
+        config.registerSubscribingEventProcessor("com.sparcs.counter.kiosk", c -> source);
+    }
 }
