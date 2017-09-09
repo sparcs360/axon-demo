297a852b383c1e0febcc4d511599df48271e577f
Lee Newfeld <lee.newfeld@gmail.com>
2017-09-09 12:05:32 +0100

    Create docker image for Eureka Server

---
+++ axon-demo-discovery/src/main/java/com/sparcs/discovery/Application.java
@@ -0,0 +1,29 @@
+package com.sparcs.discovery;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class Application {
+	
+	private static final Logger LOG = LoggerFactory.getLogger(Application.class);
+	
+	public static void main(String[] args) {
+
+        SpringApplication.run(Application.class, args);        
+	}
+	
+	@Bean
+	CommandLineRunner initialise() {
+		
+		return((args) -> {
+			LOG.info("Discovery Server Ready");
+		});
+	}
+}
